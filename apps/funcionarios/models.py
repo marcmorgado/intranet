2b09887dc981a66eb3717b425900c6991e5272fc
Marcelo <marcelo.morgado1974mm@gmail.com>
2020-08-13 11:20:43 -0300

    Criando relacoes

--- apps/funcionarios/models.py
+++ apps/funcionarios/models.py
@@ -1,7 +1,14 @@
 from django.db import models
+from django.contrib.auth.models import User
+from apps.setores.models import Setor
+from apps.unidade.models import Unidade
 
 class Funcionario(models.Model):
     nome = models.CharField(max_length=100)
+    user = models.OneToOneField(User, on_delete=models.PROTECT)
+    setores = models.ManyToManyField(Setor)
+    unidade = models.ForeignKey(Unidade, on_delete=models.PROTECT)
+
 
 def __str__(self):
         return self.nome
--- apps/unidade/models.py
+++ apps/unidade/models.py
@@ -2,3 +2,6 @@ from django.db import models
 
 class Unidade(models.Model):
     nome = models.CharField(max_length=100, help_text='Nome da Unidade')
+
+    def __str__(self):
+        return self.nome
